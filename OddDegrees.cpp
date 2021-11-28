a6368be2a845fbd22c4acf794055c47cc73b6b35
Crow <32833323+CoolKidCrow@users.noreply.github.com>
2021-11-27 20:20:55 -0700

    OddDegrees file

---
+++ OddDegrees.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Size-Matters on 11/27/2021.
+//
+
+#include "OddDegrees.h"
---
+++ OddDegrees.h
@@ -0,0 +1,14 @@
+//
+// Created by Size-Matters on 11/27/2021.
+//
+
+#ifndef CSE310HW07_ODDDEGREES_H
+#define CSE310HW07_ODDDEGREES_H
+
+
+class OddDegrees {
+
+};
+
+
+#endif //CSE310HW07_ODDDEGREES_H
